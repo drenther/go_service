15907fd705ead282e8818d2e7f9859234c6067dc
Soumyajit Pathak <s.pathak5995@gmail.com>
2019-11-22 23:12:23 +0530

    fixes and cleanup

--- main.go
+++ main.go
@@ -11,25 +11,28 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-type Task struct {
-	Id   string `json:"id"`
-	User string `json:"user"`
-	Body string `json:"body"`
-}
-
 var port = ":4021"
 var dbName = "tasks"
 
-var db scribble.Driver
+type ContextWithDB struct {
+	echo.Context
+	Db *scribble.Driver
+}
 
 func main() {
-	d, dbErr := scribble.New(dbName, nil)
+	db, dbErr := scribble.New(dbName, nil)
 	if dbErr != nil {
 		log.Fatal("Failed to connect to database", dbErr)
 	}
-	db = *d
 
 	server := echo.New()
+
+	server.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			context := &ContextWithDB{c, db}
+			return next(context)
+		}
+	})
 
 	server.GET("/", getTasks)
 
@@ -38,38 +41,56 @@ func main() {
 	server.Logger.Fatal(server.Start(port))
 }
 
-func createTask(context echo.Context) (err error) {
-	user := strings.Split(strings.ToLower(context.Request().Header.Get("Authorization")), " ")[1]
+type Task struct {
+	Id   string `json:"id"`
+	User string `json:"user"`
+	Body string `json:"body"`
+}
+
+func getUser(authHeader string) string {
+	return strings.Split(strings.ToLower(authHeader), " ")[1]
+}
+
+func createTask(c echo.Context) (err error) {
+	context := c.(*ContextWithDB)
+
+	user := getUser(context.Request().Header.Get("Authorization"))
 	id := uuid.NewV4().String()
 	task := &Task{
 		Id:   id,
 		User: user,
 	}
 
-	if err = db.Write(dbName, id, &task); err != nil {
+	if err = context.Bind(task); err != nil {
 		return
 	}
 
-	if err = context.Bind(task); err != nil {
+	if err = context.Db.Write(dbName, id, &task); err != nil {
 		return
 	}
 
 	return context.JSON(http.StatusCreated, task)
 }
 
-func getTasks(context echo.Context) (err error) {
-	records, err := db.ReadAll(dbName)
-	if err != nil {
-		return
-	}
+func getTasks(c echo.Context) (err error) {
+	context := c.(*ContextWithDB)
+	user := getUser(context.Request().Header.Get("Authorization"))
 
 	tasks := []Task{}
+	records, err := context.Db.ReadAll(dbName)
+	if err != nil {
+		return context.JSON(http.StatusNoContent, tasks)
+	}
+
 	for _, f := range records {
 		task := Task{}
 		if err = json.Unmarshal([]byte(f), &task); err != nil {
 			return
 		}
-		tasks = append(tasks, task)
+
+		if user == task.User {
+			tasks = append(tasks, task)
+		}
 	}
 
 	return context.JSON(http.StatusOK, tasks)
